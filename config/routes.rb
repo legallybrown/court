615584986c541d06098a5fb9f42de63e3f4b6c83
Anoop Joshi <anoopjoshi@hotmail.co.uk>
2014-07-08 23:53:37 +0100

    create welcome index controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Court::Application.routes.draw do
+  get "welcome/index"
+  
+  root 'welcome#index'
+
   devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
