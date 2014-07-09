ad215fb175e2ff115fbf38406f23a5be5b1c8a92
Anoop Joshi <anoopjoshi@hotmail.co.uk>
2014-07-09 08:59:13 +0100

    import 50 most recent cases with nokogiri

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,2 +1,12 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
 class Import < ActiveRecord::Base
+
+  def self.import_most_recent
+    url = "http://www.scotcourts.gov.uk/search-judgments/court-of-session"
+    @doc = Nokogiri::HTML(open(url))
+  end
+
 end
