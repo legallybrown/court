582cd3eba1135188674f6f5797da0912d144242a
Anoop Joshi <anoopjoshi@hotmail.co.uk>
2014-07-09 08:58:47 +0100

    import references, links and judges

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    @import = Import.import_most_recent
+
+    @cases = @import.css("td:nth-child(1)").map {|item| item.at_css("a").text}
+
+    @links = @import.css("td:nth-child(1)").map {|item| item.at_css("a")[:href]}
+
+    @judges = @import.css("td:nth-child(4)").map {|item| item.at_css(".judge").text}
+
   end
 end
